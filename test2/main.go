5c684b52bfed1379df1c821c026d5738cc6b8b0a
Phu Nguyen <phu.nguyen2@sotatek.com>
2022-07-11 09:58:38 +0700

    second  commit

--- test2/main.go
+++ test2/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"strconv"
 	"sync"
 	"time"
@@ -9,6 +12,18 @@ import (
 
 var M map[string]string
 var mu sync.Mutex
+
+func chanRoutine() {
+	log.Print("hello 1")
+	pipe := make(chan string, 10)
+	go func() {
+		time.Sleep(1 * time.Second)
+		pipe <- "hello 3"
+		receiver := <-pipe
+		log.Print(receiver)
+	}()
+	log.Print("hello 2")
+}
 
 func Input(a int, b int) {
 	M = make(map[string]string)
@@ -23,6 +38,51 @@ func Input(a int, b int) {
 
 func Output() {
 	fmt.Print(M)
+}
+
+func errFunc() {
+	m := make(map[int]int)
+	for i := 0; i < 1000; i++ {
+		go func() {
+			for j := 1; j < 10000; j++ {
+				mu.Lock()
+				if _, ok := m[j]; ok {
+					delete(m, j)
+					continue
+				}
+				m[j] = j * 10
+				mu.Unlock()
+			}
+		}()
+	}
+
+	log.Print("done")
+}
+
+func Test4() {
+	var file, err = os.Open("file.txt")
+	if err != nil {
+		fmt.Print("Error opening file.txt: ", err)
+	}
+	defer file.Close()
+
+	c := make(chan string, 10)
+	index := 0
+
+	scanner := bufio.NewScanner(file)
+	mu.Lock()
+	for scanner.Scan() {
+		go func() {
+			//time.Sleep(1 * time.Second)
+			c <- scanner.Text()
+			index++
+		}()
+		fmt.Printf("So dong hien tai: %d co gia tri la: %s \n", index, <-c)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Print("Error reading file.txt: ", err)
+	}
+	mu.Unlock()
 }
 
 func main() {
